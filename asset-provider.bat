2a52c62195d66d133711cda4836215465b965e1a
Van Vo <vvo@milestone.dk>
2024-01-31 17:21:30 +0200

    checking runners

---
+++ asset-provider.bat
@@ -0,0 +1,23 @@
+@echo off
+set url=%1
+set sha512=%2
+set dst=%3
+set "_dst=%dst:/=\%"
+set "_sha512=%sha512:~0,90%"
+
+cd /d %~dp3
+%NUGET% install %sha512:~0,90% -Source https://nuget.pkg.github.com/van-off-the-ovvo/index.json
+echo.
+if exist %_sha512%.1.0.0 (
+    echo "Pull from the NuGet feed"
+    cd %_sha512%.1.0.0
+
+    REM Assume both are files not directories
+    echo "F" | xcopy /f *.part %_dst%
+) else (
+    echo "Fetch from the url"
+    curl.exe -L %url% --create-dirs --output %dst%
+    REM Replace with the correct path
+    %NUGET% pack C:\path\to\asset-source.nuspec -BasePath %~dp3 -Properties "sha=%_sha512%;file=%dst%" -OutputDirectory %TEMP%
+    %NUGET% push -ApiKey az -SkipDuplicate %TEMP%\%_sha512%.1.0.0.nupkg -Source https://nuget.pkg.github.com/van-off-the-ovvo/index.json
+)
